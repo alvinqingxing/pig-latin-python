5b78c5ff12103f2e5a87855772304bb2dd57a37a
alvinqingxing <alvinch@hawaii.edu>
2021-02-01 09:27:02 +0800

    Pig Latin

--- pig_latin.py
+++ pig_latin.py
@@ -1,6 +1,6 @@
 def pig_latin(sentence):
-  pig_latin_text = []
-  word_list = sentence.split()
-  for word in word_list:
-    pig_latin_text.append(word[1:] + word[0] + "ay")
-  return " ".join(pig_latin_text)
+	pig_latin_text = []
+	word_list = sentence.split()
+	for word in word_list:
+		pig_latin_text.append(word[1:] + word[0] + "ay")
+	return " ".join(pig_latin_text)
